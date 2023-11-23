8a0cca1febedca7ba0d97927cfcbb0fdb2f4b101
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2023-11-23 11:06:00 +0000

    Add files via upload

---
+++ matmul2d.c
@@ -0,0 +1,100 @@
+#include<mpi.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define MAXSIZE 32			/* change the size of matrices as required*/
+
+int X[MAXSIZE][MAXSIZE], Y[MAXSIZE][MAXSIZE], Z[MAXSIZE][MAXSIZE];
+
+void fill_matrix(int mat[MAXSIZE][MAXSIZE]){
+    srand(time(NULL));
+	for(int i = 0; i < MAXSIZE; i++){
+        for(int j = 0; j < MAXSIZE; j++){
+            mat[i][j] = rand() % 10;
+        }
+    }
+}
+
+void print_matrix(int mat[MAXSIZE][MAXSIZE])
+{
+	for(int i = 0; i < MAXSIZE; i++){
+        for(int j = 0; j < MAXSIZE; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    double tStart, tEnd;
+
+    int my_rank, comm_size, from, to;
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+   
+    
+    int part_size = MAXSIZE*MAXSIZE/comm_size;
+
+    int localX[part_size][MAXSIZE], localY[part_size][MAXSIZE], localZ[part_size][MAXSIZE];
+
+    int i, j, k;
+    int root = 0;
+
+    tStart = MPI_Wtime();
+    
+	if(MAXSIZE % comm_size != 0){
+		exit(-1);
+	}
+
+    from = my_rank * part_size;
+    to = (my_rank+1) * part_size;
+
+    printf("my_rank %d, from: %d to: %d", my_rank, from, to);
+
+    /*if root rank, fill the matrices X and Y*/
+
+    if(my_rank == root){
+        fill_matrix(X);
+        fill_matrix(Y);
+        //print_matrix(Y);
+        //print_matrix(X);
+    }
+  
+    /*What's the difference here between MPI_Bcast and MPI_Scatter*/
+    MPI_Bcast(Y, MAXSIZE*MAXSIZE, MPI_INT, root, MPI_COMM_WORLD);
+
+    
+
+    MPI_Scatter (X, part_size * MAXSIZE, MPI_INT, localX, part_size * MAXSIZE, MPI_INT, root, MPI_COMM_WORLD);
+    
+
+    /*parallelise here using OpenMP: fastest time wins!!!! Use clauses and anythign at your disposal. Change the code if you want to.
+    Consider NUMA, consider the chunk size of your schedules. Experiment!!!!!!!*/
+    for (i=from; i<to; i++){ 
+        for (j=0; j<MAXSIZE; j++) {
+            localZ[i][j]=0;
+            for (k=0; k<MAXSIZE; k++){
+                localZ[i][j] += localX[i][k]*Y[k][j];
+            }
+        }
+    }
+
+
+    MPI_Gather(localZ, part_size * MAXSIZE, MPI_INT, Z, part_size * MAXSIZE, MPI_INT, root, MPI_COMM_WORLD);
+
+    /*if root print mat Z*/
+    if (my_rank == 0){
+        print_matrix(Z);
+    }
+
+    tEnd = MPI_Wtime();
+
+    printf("\nProgram took %lf milliseconds\n", (tEnd - tStart) * 1000);
+
+    MPI_Finalize();
+    return 0;
+}
